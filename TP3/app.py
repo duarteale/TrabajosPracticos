091b03f8f876b494f87122ef414a2c178c9b4edd
Ale <duartealejandrame@gmail.com>
2023-06-26 20:00:08 -0300

    Agregamos un paginador. Elias, Martin, Julian y Ale

--- TP3/app.py
+++ TP3/app.py
@@ -5,53 +5,57 @@
 # guardar_lista_diccionarios_como_json para que me mantenga los datos y no los sobreescriba, o bien arbitrar
 # un metodo de guardado que cumpla la misma funcion. 
 
-
 from flask import Flask, render_template, request, redirect
 from uuid import uuid4
+import json
 
 app = Flask(__name__)
 
 # Lista de tareas (simulación de una base de datos)
 tasks = [
-    {"id": 1, "title": "Tarea 1", "description": "Descripción de la tarea 1"},
-    {"id": 2, "title": "Tarea 2", "description": "Descripción de la tarea 2"},
-    {"id": 3, "title": "Tarea 3", "description": "Descripción de la tarea 3"}
+    {"id": str(uuid4()), "title": "Tarea 1", "description": "Descripción de la tarea 1"},
+    {"id": str(uuid4()), "title": "Tarea 2", "description": "Descripción de la tarea 2"},
+    {"id": str(uuid4()), "title": "Tarea 3", "description": "Descripción de la tarea 3"}
 ]
 next_task_id = 4  # ID de la próxima tarea a crear
 
-
-@app.route("/")
+loged= False
+@app.route("/", methods=["GET", "POST"])
+@app.route("/inicio")
 def index():
-    return render_template("index.html", tasks=tasks)
+    global loged
+    if request.method == "POST":
+        loged = not loged    
+    return render_template("index.html", tasks=tasks, loged=loged)
 
 
 @app.route("/create", methods=["GET", "POST"])
 def create():
     global next_task_id
-
+    global loged
     if request.method == "POST":
         title = request.form["title"]
         description = request.form["description"]
-        task = {"id": next_task_id, "title": title, "description": description}
+        task = {"id": str(uuid4()), "title": title, "description": description}
         tasks.append(task)
-        next_task_id += 1
+        next_task_id += 1        
         return redirect("/")
     else:
-        return render_template("create.html")
+        return render_template("create.html", loged=loged)
 
 
 @app.route("/edit/<int:task_id>", methods=["GET", "POST"])
 def edit(task_id):
-
+    global loged
     isNotExist = False
     for task in tasks:
-        if task["id"] == task_id:
+        if task["id": str(uuid4())] == task_id:
             if request.method == "POST":
                 task["title"] = request.form["title"]
                 task["description"] = request.form["description"]
                 return redirect("/")
             else:
-                return render_template("edit.html", task=task)
+                return render_template("edit.html", task=task, loged=loged)
         else:
             isNotExist = True
     
@@ -72,7 +76,11 @@ def edit(task_id):
 @app.route("/delete/<int:task_id>")
 def delete(task_id):
     global tasks
-    tasks = [task for task in tasks if task["id"] != task_id]
+    updated_tasks = []
+    for task in tasks:
+        if task["id"] != str(task_id):
+            updated_tasks.append(task)
+    tasks = updated_tasks
     return redirect("/")
 
 
